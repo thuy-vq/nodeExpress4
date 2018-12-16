e1e45ea3345a173b622ad20488444e9cfe87f00e
thuy-vq <thuy.vq@hotmail.com>
2018-12-16 22:52:49 +0700

    Setting up toolings

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var morgan = require('morgan');
-var path = require('path');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
@@ -13,11 +14,11 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/popper.js/dist')));
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
   // res.send('Hello from my library app');
   res.sendFile(path.join(__dirname, 'views/index.html'));
-})
+});
 
-app.listen(3000, function(){
-  debug(`listening on port ${chalk.green('3000')}`);
+app.listen(port, () => {
+  debug(`listening on port ${chalk.green(port)}`);
 });
