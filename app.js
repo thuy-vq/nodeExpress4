a53811d41d1350995395991215733681b7495913
thuy-vq <thuy.vq@hotmail.com>
2018-12-18 22:26:54 +0700

    templating engines - ejs

--- app.js
+++ app.js
@@ -4,24 +4,35 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
-app.use('/js', express.static(path.join(__dirname, '/node_modules/popper.js/dist')));
 app.set('views', './src/views');
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
+const nav = [
+  { link: '/books', title: 'Book' },
+  { link: '/authors', title: 'Author' }
+];
+
+const bookRouter = require('./src/routes/bookRoutes')(nav);
+
+app.use('/books', bookRouter);
 app.get('/', (req, res) => {
-  // res.send('Hello from my library app');
-  // res.sendFile(path.join(__dirname, 'views/index.html'));
-  res.render('index', {
-    list: ['a', 'b']
-  });
+  res.render(
+    'index',
+    {
+      nav: [{ link: '/books', title: 'Books' },
+        { link: '/authors', title: 'Authors' }],
+      title: 'Library'
+    }
+  );
 });
 
 app.listen(port, () => {
