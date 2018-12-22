b2c1a7399861539ee8eb26835495bae87a88c31c
thuy-vq <thuy.vq@hotmail.com>
2018-12-22 17:59:32 +0700

    connect to SQL

--- app.js
+++ app.js
@@ -4,11 +4,31 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 
-
 const app = express();
 const port = process.env.PORT || 3000;
 
+const sql = require('mssql');
+
+const config = {
+  user: 'sa',
+  password: 'VQT@123',
+  server: 'DELL-PC\\SQLEXPRESS', // You can use 'localhost\\instance' to connect to named instance
+  database: 'PSLibrary',
+
+  options: {
+    encrypt: true // Use this if you're on Windows Azure
+  }
+}
+
+sql.connect(config)
+  .then(result => debug(`SQL connected ${result};`))
+  .catch(err => debug(`failedd ${err};`));
+
 app.use(morgan('tiny'));
+app.use((req, res, next) => {
+  debug('my middleware');
+  next();
+})
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
@@ -22,8 +42,11 @@ const nav = [
 ];
 
 const bookRouter = require('./src/routes/bookRoutes')(nav);
+const adminRouter = require('./src/routes/adminRoutes')(nav);
 
 app.use('/books', bookRouter);
+app.use('/admin', adminRouter);
+
 app.get('/', (req, res) => {
   res.render(
     'index',
---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,119 @@
+const express = require('express');
+// const { MongoClient, ObjectID } = require('mongodb');
+const debug = require('debug')('app:bookRoutes');
+
+const bookRouter = express.Router();
+const sql = require('mssql');
+
+function router(nav) {
+  bookRouter.route('/')
+    .get((req, res) => {
+      (async function query() {
+        try {
+          const request = new sql.Request();
+          const { recordset } = await request.query('select * from books');
+          res.render(
+            'bookListView',
+            {
+              nav,
+              title: 'Library',
+              books: recordset
+            }
+          );
+        } catch (err) {
+          debug(err.stack);
+        }
+      }());
+
+
+
+
+      // const url = 'mongodb://localhost:27017';
+      // const dbName = 'libraryApp';
+
+      // (async function mongo() {
+      //   let client;
+      //   try {
+      //     client = await MongoClient.connect(url);
+      //     debug('Connected correctly to server');
+      //
+      //     const db = client.db(dbName);
+      //
+      //     const col = await db.collection('books');
+      //
+      //     const books = await col.find().toArray();
+      //
+      //     res.render(
+      //       'bookListView',
+      //       {
+      //         nav,
+      //         title: 'Library',
+      //         books
+      //       }
+      //     );
+      //   } catch (err) {
+      //     debug(err.stack);
+      //   }
+      //   client.close();
+      // }());
+    });
+
+  bookRouter.route('/:id')
+    .all((req, res, next) => {
+      const { id } = req.params;
+      (async function query() {
+        try {
+          const request = new sql.Request();
+          const { recordset } = await request.input('id', sql.Int, id)
+            .query('select * from books where id = @id');
+          [req.book] = recordset;
+          next();
+        } catch (err) {
+          debug(err.stack);
+        }
+      }());
+    })
+    .get((req, res) => {
+      res.render(
+        'bookView',
+        {
+          nav,
+          title: 'Library',
+          book: req.book
+        }
+      );
+
+
+      // const url = 'mongodb://localhost:27017';
+      // const dbName = 'libraryApp';
+
+      // (async function mongo() {
+      //   let client;
+      //   try {
+      //     client = await MongoClient.connect(url);
+      //     debug('Connected correctly to server');
+      //
+      //     const db = client.db(dbName);
+      //
+      //     const col = await db.collection('books');
+      //
+      //     const book = await col.findOne({ _id: new ObjectID(id) });
+      //     debug(book);
+      //     res.render(
+      //       'bookView',
+      //       {
+      //         nav,
+      //         title: 'Library',
+      //         book
+      //       }
+      //     );
+      //   } catch (err) {
+      //     debug(err.stack);
+      //   }
+      // }());
+    });
+  return bookRouter;
+}
+
+
+module.exports = router;
